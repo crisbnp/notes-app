e4963de43bc40028599f0006864b6043d2bdb744
Cristien Natal <crisbnp@icloud.com>
2019-05-29 01:00:10 +0100

    text area component

--- src/App.js
+++ src/App.js
@@ -1,48 +1,36 @@
 import React, { Component } from 'react';
+import Textarea from './components/Textarea'
+
+
 import Header from './components/Header';
 import NoteCard from './components/NoteCard'
 import './App.css';
 
 class App extends Component {
   state = {
-    title: prompt('Name your notes!'),
-    note : '',
     notes: [],
-    time: ''
   }
 
-  handleDelete = (event) => {
-    
+  handleSubmit = (data) => {
+    let notes = JSON.parse(localStorage.getItem('notes'))
+    let newNotes = JSON.stringify([...notes, data])
+    localStorage.setItem('notes', newNotes)
   }
 
-  handleAdd = (event) => {
-    const {notes, note} = this.state
-    let newNote = notes.push(note)  
-    this.setState({
-      notes: newNote
-    })
-  }
-
-  handleEdit = (event) => {
-    this.setState({
-
-    })
-  }
-
-  handleNote = (event) => {
-    this.setState({
-      note: event.target.value
-    })
+  componentDidMount () {
+    let notes = localStorage.getItem('notes')
+    if (!notes) {
+      localStorage.setItem('notes', JSON.stringify([]))
+    }
   }
 
   render() {
     const {title, getTime, note} = this.state
 
     return (
+  
       <div className="App">
-        <Header title={title}></Header>
-        <button onClick={this.handleAdd}>Add New Note</button>
-        <NoteCard handleDelete={this.handleDelete} handleEdit={this.handleEdit} timestamp={getTime} yourNotes={note}></NoteCard>
+            <Textarea onSubmit={this.handleSubmit}/>
       </div>
     );
   }
