83c02d1466465aed8c1d713ff2314f9fe9cce77e
Cristien Natal <crisbnp@icloud.com>
2019-06-01 01:14:47 +0100

    Styled components on textarea

--- src/components/Textarea.jsx
+++ src/components/Textarea.jsx
@@ -2,6 +2,36 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import nanoid from 'nanoid';
 
+const Input = styled.input`
+  padding: 0.5em;
+  margin: 0.5em;
+  color: ${props => props.inputColor || "palevioletred"};
+  background: papayawhip;
+  border: none;
+  border-radius: 3px;
+`;
+
+const Textarea = styled.textarea`
+padding: 0.5em;
+  margin: 0.5em;
+  color: ${props => props.inputColor || "palevioletred"};
+  background: rebeccapurple;
+  border: none;
+  border-radius: 3px;
+
+`
+
+const Button = styled.button`
+  background: ${props => props.primary ? "palevioletred" : "white"};
+  color: ${props => props.primary ? "white" : "palevioletred"};
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+
+
 class Textarea extends Component {
 
     state = {
@@ -31,20 +61,20 @@ class Textarea extends Component {
 
         return (
             <div>
-                <input
+                <Input
                     name="title"
                     type="text"
                     value={this.state.title}
                     onChange={this.onChange}
                 />
-                <textarea
+                <Textarea
                     name="content"
                     rows='4'
                     cols='20'
                     value={this.state.content}
                     onChange={this.onChange}
                 />
-                <button onClick={this.onSubmit}>Save</button>
+                <Button onClick={this.onSubmit}>Save</Button>
             </div>
         )
     }
